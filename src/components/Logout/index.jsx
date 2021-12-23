96a31703b0c63a5ef82b43148894092cbda168fd
VivienShen <vivienshen88@gmail.com>
2021-12-23 15:33:30 -0500

    create logout page and link user to firebase

---
+++ src/components/Logout/index.jsx
@@ -0,0 +1,36 @@
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+
+export const Logout = () => {
+
+    const [user, setUser] = useState(null);
+
+    //display button only if there's a current logged in user
+    useEffect(
+        () => {
+            const auth = getAuth();
+            onAuthStateChanged( auth, (user) => {
+                if(user) {
+                    setUser(user);
+                } else {
+                    setUser(null);
+                }
+            })
+        }, []
+    )
+
+    const logoutUser = async() => {
+        const auth = getAuth();
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        user && <button className="logout-btn" onClick={logoutUser}>
+            Logout
+        </button>
+    )
+}
